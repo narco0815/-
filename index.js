78f3243f5ec6df797756ea57e9bc6810ea038d1c
narco0815 <65471018+narco0815@users.noreply.github.com>
2021-04-18 11:43:59 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let  name = "shinya" ;
+console.log(name);
+console.log(window);
+
+document.getElementsByTagName("button")[0].addEventListener("click",()=>{
+    window.alert('まいちゃんおつかれさま、ドングリありがとう');
+});
+
+document.getElementsByTagName("button")[1].addEventListener("click",()=>{
+    window.alert('わんわん');
+});
+
+document.getElementsByTagName("button")[2].addEventListener("click",()=>{
+    window.alert('おつかれさま、いっぱいやすんでね');
+});
+
+document.getElementsByTagName("button")[3].addEventListener("click",()=>{
+    window.alert('にゃー');
+});
+
+document.getElementsByTagName("button")[4].addEventListener("click",()=>{
+    window.alert("お疲れ様！僕たちもアニバーサリー？僕の給料でご飯連れっってあげるからね！！　これからの時代、好きなこと、やりたいことをできるのが、それもまた強さだと思います。大変な時、何か落ち込む時などきっとその強さが助けてくれると思います。とにかくお疲れ様！");
+
+});
